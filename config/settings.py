a80e759cca03f10ef7a2527ab67421ad4b5c2c8c
Assimiu-a21804689 <assimium2@gmail.com>
2021-05-28 10:29:29 +0100

    novo

--- config/settings.py
+++ config/settings.py
@@ -41,9 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'website',
     'whitenoise.runserver_nostatic',
-    'django.contrib.staticfiles',
+    'website',
 ]
 
 MIDDLEWARE = [
